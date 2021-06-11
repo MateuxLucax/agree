a4b0047151e182fcc8f7978afc4accf26e7451dd
Lucas M. Schwambach <48414967+luc527@users.noreply.github.com>
2021-06-10 23:52:07 -0300

    removed some unnecessary comments

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -24,17 +24,17 @@ public class Application {
     private JPanel homePanel;
 
     public Application() {
-        this.loggedUserGroups = new ArrayList<>();
-        this.loggedUserFriends = new ArrayList<>();
+        loggedUserGroups = new ArrayList<>();
+        loggedUserFriends = new ArrayList<>();
 
-        this.frame = new JFrame();
+        frame = new JFrame();
         frame.addWindowListener(new WindowAdapter() {
             public void windowClosing(WindowEvent e) {
                 System.exit(0);
             }
         });
 
-        this.homePanel = new JPanel();
+        homePanel = new JPanel();
 
         startSession();
     }
@@ -83,62 +83,7 @@ public class Application {
         homePanel.add(friendsPanel);
     }
 
-
-    /*public static Application startSession()
-    throws IncorrectPasswordException,
-           NameAlreadyInUseException,
-           UnsafePasswordException
-    {
-        // (For now we'll use a Scanner, just as a placeholder)
-        Scanner scan = new Scanner(System.in);
-
-        System.out.println("Login (type 1) or create account (type 2)?");
-        int action = scan.nextInt();
-
-        System.out.print("What's your name? ");
-        String name = scan.next();
-        System.out.print("What's your password? ");
-        String password = scan.next();
-        Application app;
-        if (action == 1) {
-            userDataAccess.authenticate(name, password);
-            User user = userDataAccess.retrieveUser(name);
-            app = new Application(user);
-            app.loggedUserGroups.addAll(userDataAccess.retrieveGroups(user));
-            app.loggedUserFriends.addAll(userDataAccess.retrieveFriends(user));
-        } else {
-            userDataAccess.createAccount(name, password);
-            User user = userDataAccess.retrieveUser(name);
-            app = new Application(user);
-        }
-
-        return app;
-    }*/
-
     public static void main(String[] args) {
-
         var app = new Application();
-
-        /* try {
-            Application app = Application.startSession();
-            System.out.println("Welcome!");
-
-
-            System.out.println("\nYour groups:");
-            app.loggedUserGroups.sort(Comparator.comparing(Group::getLastMessageDate));
-            System.out.println(app.loggedUserGroups);
-
-            System.out.println("\nYour friends:");
-            System.out.println(app.loggedUserFriends);
-
-        } catch (IncorrectPasswordException e) {
-            System.out.println("Invalid credentials for user " + e.getUsername());
-        } catch (UnsafePasswordException e) {
-            System.out.println("Your password is unsafe -- too short, lacking numbers etc.");
-        } catch (NameAlreadyInUseException e) {
-            System.out.println("The name " + e.getName() + " is already being used by someone else");
-        } catch(Exception e) {
-            e.printStackTrace();
-        } */
     }
 }
