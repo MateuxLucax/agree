c26d89a3a23ef8669e47702947d7186e0c7fc0cf
Lucas M. Schwambach <48414967+luc527@users.noreply.github.com>
2021-06-11 17:57:00 -0300

    gui: GroupPanel loads the most recent messages

--- src/main/java/gui/GroupPanel.java
+++ src/main/java/gui/GroupPanel.java
@@ -2,27 +2,50 @@ package gui;
 
 // Where the user actually reads and writes messages
 
+import data.GroupDataAccess;
 import models.group.Group;
 
 import javax.swing.*;
+import javax.swing.border.Border;
+import java.awt.*;
 import java.awt.event.ActionListener;
 
 public class GroupPanel {
 
     private JPanel mainPanel;
+    private JPanel headerPanel;
     private JLabel lbName;
     private JLabel lbLastMessage;
     private JButton btGoBack;
 
+    private static final GroupDataAccess groupDataAccess = GroupDataAccess.getInstance();
+
     public GroupPanel(Group group) {
+        groupDataAccess.populateUsers(group);
+        groupDataAccess.loadMostRecentMessages(group);
+
         lbName = new JLabel(group.getName());
         lbLastMessage = new JLabel(group.getLastMessageDate().toString());
         btGoBack = new JButton("Go back");
 
+        headerPanel = new JPanel();
+        headerPanel.add(lbName);
+        headerPanel.add(lbLastMessage);
+        headerPanel.add(btGoBack);
+
+        var messagesPanel = new JPanel();
+        messagesPanel.setLayout(new BoxLayout(messagesPanel, BoxLayout.PAGE_AXIS));
+        for (var msg : group.getMessages())
+            messagesPanel.add(new MessagePanel(msg).getPanel());
+        var messagesScrollPane = new JScrollPane();
+        messagesScrollPane.setViewportView(messagesPanel);
+        messagesScrollPane.getVerticalScrollBar().setUnitIncrement(20);
+
         mainPanel = new JPanel();
-        mainPanel.add(lbName);
-        mainPanel.add(lbLastMessage);
-        mainPanel.add(btGoBack);
+        mainPanel.setLayout(new BorderLayout());
+        mainPanel.add(messagesScrollPane, BorderLayout.CENTER);
+        mainPanel.add(headerPanel, BorderLayout.PAGE_START);
+
     }
 
     public void setGoBackListener(ActionListener onGoBack) {
