1fad7445ba550e0ef4ee52b6097afeee0204211c
Lucas M. Schwambach <48414967+luc527@users.noreply.github.com>
2021-06-18 18:03:25 -0300

    gui: now using JTabbedPane instead of reinventing tabs ourselves

--- src/main/java/app/Application.java
+++ src/main/java/app/Application.java
@@ -3,21 +3,20 @@ package app;
 import com.github.weisj.darklaf.LafManager;
 import com.github.weisj.darklaf.theme.DarculaTheme;
 import com.github.weisj.darklaf.theme.laf.DarculaThemeDarklafLookAndFeel;
-import gui.*;
+import gui.AuthPanel;
+import gui.GroupPanel;
 import utils.AssetsUtil;
 
 import javax.swing.*;
-import java.awt.*;
-import java.awt.event.WindowAdapter;
-import java.awt.event.WindowEvent;
 
 public class Application {
 
     private UserSession userSession;
 
-    private final AppFrame frame;
+    private final JFrame frame;
 
     public Application() {
+        // Initialize theme
         LafManager.install();
         LafManager.setTheme(new DarculaTheme());
         try {
@@ -26,20 +25,16 @@ public class Application {
             e.printStackTrace();
         }
 
-        frame = new AppFrame("Agree");
+        frame = new JFrame("Agree");
         frame.setIconImage(AssetsUtil.getImage(AssetsUtil.ICON));
-        frame.addWindowListener(new WindowAdapter() {
-            public void windowClosing(WindowEvent e) {
-                System.exit(0);
-            }
-        });
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 
         startSession();
     }
 
     private void startSession() {
         var authPanel = new AuthPanel();
-        frame.setMainPanel(authPanel.getJPanel());
+        frame.add(authPanel.getJPanel());
 
         frame.setExtendedState(frame.getExtendedState() | JFrame.MAXIMIZED_BOTH);
         //frame.getContentPane().setPreferredSize(Toolkit.getDefaultToolkit().getScreenSize());
@@ -49,77 +44,40 @@ public class Application {
         frame.setVisible(true);
 
         authPanel.setSuccessListener(() -> {
-            frame.setMainPanel(new JPanel());  // empty panel just to replace the authPanel
+            frame.remove(authPanel.getJPanel());
             userSession = UserSession.getInstance();
             initializeFrame();
+            frame.pack();
             frame.revalidate();
         });
     }
 
     private void initializeFrame() {
-        /*
-                                         mainPanel
-                   sideBar               VVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVV
-                   VVVVVVVVVVVVVVVVVVVVVV
-                   +---------------------------------------------------------------------+
-                   | > groups | friends | group 2 -- last message 10 min ago             |
-                   +----------+---------+                                                |
-        sidePanel {| group 1            | ...                                            |
-                  {| > group 2          | dude_n (15 min ago): asdfasdf                  |
-                  {| group 3            | dude_2 (14 min ago): goodbye                   |
-                  {| ...                | dude_1 (10 min ago): hello world               |
-                   +--------------------+                                                |
-                   | create group       | Write message: _______________________________ |
-                   +--------------------+------------------------------------------------+
-        User clicks on "groups" or "friends" -> Contents of the sidePanel changes
-        User clicks on a group, on a friend, or on "create group" -> Contents of the mainPanel change
-        */
-
-        var groupListPanel = new JPanel();
-        groupListPanel.setLayout(new BoxLayout(groupListPanel, BoxLayout.PAGE_AXIS));
-
+        // For now the tabs only have text, but we can add icons to them
+        // when we implement groups and users having their profile pictures:
+        // https://stackoverflow.com/q/17648780
+        var groupListPanel = new JTabbedPane(JTabbedPane.LEFT);
         for (var group : userSession.getGroups()) {
-            var groupPanel = new GroupPanel(group);
-            var groupBar = new GroupBar(group);
-            groupBar.setOpenListener(evt -> {
-                frame.setMainPanel(groupPanel.getJPanel());
-                frame.revalidate();
-                frame.pack();
-            });
-            groupListPanel.add(groupBar.getJPanel());
+            groupListPanel.addTab(group.getName(), new GroupPanel(group).getJPanel());
         }
-        // FIXME when clicking on "open" in a GroupBar the group doesn't always immediatly open on the mainPanel, it's weird
-        // More specifically, it opens immediatly the first time you click it,
-        // but on further clicks you need to resize the window for it to happen
 
-        var friendListPanel = new JPanel();
-        friendListPanel.setLayout(new BoxLayout(friendListPanel, BoxLayout.PAGE_AXIS));
-
+        var friendListPanel = new JTabbedPane(JTabbedPane.LEFT);
         for (var friend : userSession.getFriends()) {
-            var friendBar = new UserBar(friend);
-            friendListPanel.add(friendBar.getJPanel());
+            var friendPanel = new JPanel();
+            // not sure what to put here yet
+            // if having someone as a friend automatically creates a group with just you and the friend only
+            // then here we'll show a GroupPanel with said group
+            friendPanel.add(new JLabel(friend.getNickname()));
+            friendListPanel.addTab(friend.getNickname(), new JPanel());  // no UserPanel yet...
         }
 
-        var btGroups = new JButton("Groups");
-        var btFriends = new JButton("Friends");
-        btGroups.addActionListener(evt -> {
-            frame.setSidePanel(groupListPanel);
-            frame.revalidate();
-        });
-        btFriends.addActionListener(evt -> {
-            frame.setSidePanel(friendListPanel);
-            frame.revalidate();
-        });
+        var sidePanel = new JTabbedPane(JTabbedPane.TOP);
+        sidePanel.addTab("Groups", groupListPanel);
+        sidePanel.addTab("Friends", friendListPanel);
 
-        var buttons = new JPanel(new FlowLayout());
-        buttons.add(btGroups);
-        buttons.add(btFriends);
+        // var btCreateGroup = new JButton("Create group");
 
-        var btCreateGroup = new JButton("Create group");
-
-        frame.addAboveSidePanel(buttons);
-        frame.setSidePanel(groupListPanel);
-        frame.addBelowSidePanel(btCreateGroup);
+        frame.add(sidePanel);
     }
 
     public static void main(String[] args) {
