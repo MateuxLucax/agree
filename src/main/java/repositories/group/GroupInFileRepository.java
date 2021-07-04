c660389b3caa0ec309791a47acc6cf29ec189e79
Lucas M. Schwambach <48414967+luc527@users.noreply.github.com>
2021-07-04 20:06:49 -0300

    GroupInFileRepository: removeGroup and updateGroup implemented

--- src/main/java/repositories/group/GroupInFileRepository.java
+++ src/main/java/repositories/group/GroupInFileRepository.java
@@ -32,12 +32,40 @@ public class GroupInFileRepository implements IGroupRepository {
 
     @Override
     public boolean removeGroup(String id) {
+        for (int i = 0; i < groups.size(); i++) {
+            if (id.equals(groups.get(i).getId())) {
+                Group removed = groups.remove(i);
+                boolean success = JsonDatabaseUtil.writeToFile(groupFile, groups);
+                if (!success)
+                    groups.add(removed);
+                return success;
+            }
+        }
         return false;
     }
 
     @Override
     public boolean updateGroup(Group group) {
-        return false;
+        // Nothing guarantees the `group' Group instance we're given is the same in memory
+        // as the one with the same id in this repository's `groups' list,
+        // so we need to take care of that:
+        int i = groups.indexOf(group);  // indexOf uses Group::equals, which compares id
+        if (i == -1)
+            return false;
+        // Make the `groups' list point to the right instance,
+        // but keep track of the previous instance in case the update fails
+        Group old = groups.get(i);
+        groups.set(i, group);
+        boolean success = JsonDatabaseUtil.writeToFile(groupFile, groups);
+        // There's a problem with this writeToFile:
+        // if other Group instances have been changed but not updated
+        // on the database, here we'll will update them too, even though
+        // this method looks like it should just update one group.
+        // I don't think this will be a problem in practice, but the
+        // static behaviour of this method is just wrong.
+        if (!success)
+            groups.set(i, old);
+        return success;
     }
 
     @Override
