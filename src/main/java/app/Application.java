664354dbb5056ce92fb32a3c02413705cdba7015
Lucas M. Schwambach <48414967+luc527@users.noreply.github.com>
2021-08-03 22:26:36 -0300

    Got rid of the UserSession

--- src/main/java/app/Application.java
+++ src/main/java/app/Application.java
@@ -19,16 +19,10 @@ public class Application {
             e.printStackTrace();
         }
 
-        // Start the session
-        // TODO get rid of UserSession, or figure out what it really is in this application, it only exists here)
-        //  maybe it centralizes access to the repositores or something?, so each Controller class
-        //  doesn't need to instance its own? idk
-        var session = UserSession.getInstance();
         var authCon = new AuthController();
         authCon.display();
         authCon.onSuccess(user -> {
-            session.initialize(user);
-            var mainCon = new MainController(session.getUser());
+            var mainCon = new MainController(user);
             mainCon.display();
             authCon.close();
         });
