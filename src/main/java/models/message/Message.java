e950f567d93d74148709caa12b6fd229b3789cfd
Lucas M. Schwambach <48414967+luc527@users.noreply.github.com>
2021-08-12 15:42:01 -0300

    Add id field to Message model

--- src/main/java/models/message/Message.java
+++ src/main/java/models/message/Message.java
@@ -7,19 +7,31 @@ import java.util.Objects;
 
 public class Message implements Comparable<Message> {
 
-    private final User user;
+    private       int    id;
+    private final User   user;
     private final String text;
-    private final Date sentAt;
+    private final Date   sentAt;
 
-    public Message(User user, String text, Date sentAt) {
-        this.user = user;
-        this.text = text;
+    public Message(int id, User user, String text, Date sentAt) {
+        this.id     = id;
+        this.user   = user;
+        this.text   = text;
         this.sentAt = sentAt;
     }
 
-    public User getUser() { return user; }
-    public Date sentAt() { return sentAt; }
+    public Message(User user, String text, Date sentAt) {
+        this.user   = user;
+        this.text   = text;
+        this.sentAt = sentAt;
+    }
+
+    public User getUser()   { return user; }
     public String getText() { return text; }
+    public Date sentAt()    { return sentAt; }
+
+    public void setId(int id) {
+        this.id = id;
+    }
 
     public int compareTo(Message other) {
         return sentAt.compareTo(other.sentAt);
@@ -27,18 +39,5 @@ public class Message implements Comparable<Message> {
 
     public String toString() {
         return String.format("Message{ %s (sent by %s at %s) }", text, user.getNickname(), sentAt.toString());
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Message message = (Message) o;
-        return Objects.equals(user, message.user) && Objects.equals(text, message.text) && Objects.equals(sentAt, message.sentAt);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(user, text, sentAt);
     }
 }
