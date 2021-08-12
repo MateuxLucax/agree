f7a4c35238f6966897cb5f9536ec98d76a89edf7
Lucas M. Schwambach <48414967+luc527@users.noreply.github.com>
2021-08-12 15:50:29 -0300

    Use specialized Group.ownedBy method

--- src/main/java/controllers/group/GroupInviteFriendsController.java
+++ src/main/java/controllers/group/GroupInviteFriendsController.java
@@ -33,7 +33,7 @@ public class GroupInviteFriendsController {
 
         for (var friend : friends) {
             // Don't show friends who are already in the group
-            if (group.getOwner().equals(friend) || members.contains(friend))
+            if (group.ownedBy(friend) || members.contains(friend))
                 continue;
 
             var bar = new InviteableFriendBar(friend.getNickname());
