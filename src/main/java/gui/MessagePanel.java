916257ca86ab76ec3d10b0e58d9aed64080cf831
Lucas M. Schwambach <48414967+luc527@users.noreply.github.com>
2021-08-13 14:57:59 -0300

    Reimplement MessagePanel + add Delete button to it

--- src/main/java/gui/MessagePanel.java
+++ src/main/java/gui/MessagePanel.java
@@ -3,24 +3,37 @@ package gui;
 import models.message.Message;
 
 import javax.swing.*;
+import java.awt.*;
 
 // TODO show profile picture of author
 
-public class MessagePanel {
-    private JPanel mainPanel;
-    private JPanel headerPanel;
-    private JLabel lbText;
-    private JLabel lbAuthor;
-    private JLabel lbSentAt;
+public class MessagePanel extends JPanel {
+
+    private JButton btDelete;
 
     public MessagePanel(Message msg) {
-        lbAuthor.setText(msg.getUser().getNickname());
-        lbSentAt.setText(msg.sentAt().toString());
-        lbText.setText(msg.getText());
+        setLayout(new BorderLayout());
+        setBorder(BorderFactory.createLineBorder(Color.BLACK));
+
+        var lbUser   = new JLabel(msg.getUser().getNickname());
+        lbUser.setBorder(BorderFactory.createEmptyBorder(2, 5, 2, 7));
+        var lbText   = new JLabel(msg.getText());
+        var lbSentAt = new JLabel(msg.sentAt().toString());  // TODO nicer looking date
+
+        add(lbUser,   BorderLayout.LINE_START);
+        add(lbText,   BorderLayout.CENTER);
+        add(lbSentAt, BorderLayout.PAGE_START);
     }
 
-    public JPanel getJPanel() {
-        return mainPanel;
+    public void showDeleteButton() {
+        btDelete = new JButton("Delete");
+        var container = new JPanel();  // Just so the button doesn't fill the whole segment
+        container.add(btDelete);
+        add(container, BorderLayout.LINE_END);
     }
-    // TODO make all these EtcPanel classes extend JPanel so that this getPanel method is unnecessary
+
+    public void onClickDelete(Runnable action) {
+        assert btDelete != null : "onClickDelete has been called but showDeleteButton hasn't";
+        btDelete.addActionListener(e -> action.run());
+    }
 }
