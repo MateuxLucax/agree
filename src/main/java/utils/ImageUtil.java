c45a7b75cb63cdb850be27eac27e80d51db53780
Lucas M. Schwambach <48414967+luc527@users.noreply.github.com>
2021-08-23 00:06:11 -0300

    Add method for retrieving *and resizing* an ImageIcon

--- src/main/java/utils/ImageUtil.java
+++ src/main/java/utils/ImageUtil.java
@@ -2,6 +2,7 @@ package utils;
 
 import javax.imageio.ImageIO;
 import javax.swing.*;
+import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.net.URL;
@@ -21,6 +22,17 @@ public class ImageUtil {
     public static ImageIcon getImageIcon(String url) {
         try {
             return new ImageIcon(ImageIO.read(new URL(url)));
+        } catch (Exception e) {
+            System.out.println("Something went wrong while loading image." + e.getMessage());
+        }
+
+        return null;
+    }
+
+    public static ImageIcon getImageIcon(String url, int width, int height) {
+        try {
+            BufferedImage img = ImageIO.read(new URL(url));
+            return new ImageIcon(img.getScaledInstance(width, height, Image.SCALE_SMOOTH));
         } catch (Exception e) {
             System.out.println("Something went wrong while loading image." + e.getMessage());
         }
